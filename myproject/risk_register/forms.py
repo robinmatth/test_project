b279dd36b3499a1c1de35b7de1e844a37153b9f9
revisionprojects <robinmatth@live.ca>
2022-01-15 20:06:48 -0500

    updated form to have bootstrap styling code

--- myproject/risk_register/forms.py
+++ myproject/risk_register/forms.py
@@ -9,3 +9,17 @@ class AddRisksForm(ModelForm):
     class Meta:
         model = Risks
         fields = ('id','risk_description','risk_mitigation','risk_owner','risk_assignee','risk_status')
+        labels = {
+            'risk_description':'',
+            'risk_mitigation':'',
+            'risk_owner':'',
+            'risk_assignee':'',
+            'risk_status':'',
+        }
+        widgets = {
+            'risk_description':forms.TextInput(attrs={'class': 'form-control', 'placeholder':'Risk Description'}),
+            'risk_mitigation':forms.TextInput(attrs={'class': 'form-control', 'placeholder':'Risk Mitigation'}),
+            'risk_owner':forms.TextInput(attrs={'class': 'form-control', 'placeholder':'Risk Owner'}),
+            'risk_assignee':forms.TextInput(attrs={'class': 'form-control', 'placeholder':'Risk Assignee'}),
+            'risk_status':forms.TextInput(attrs={'class': 'form-control', 'placeholder':'Risk Status'}),
+        }
