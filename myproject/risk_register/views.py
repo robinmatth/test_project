4f9da3d1a0129e60aa0ae19aac6e2eefba106eb5
revisionprojects <robinmatth@live.ca>
2022-01-20 01:04:37 -0500

    improved PDF export

--- myproject/risk_register/views.py
+++ myproject/risk_register/views.py
@@ -5,14 +5,15 @@ import django_tables2 as tables
 from .models import Risks
 from django.template import loader
 from .forms import AddRisksForm
-import csv, io, datetime,http
+import csv, io, datetime,http,os
 from django.http import FileResponse
 from django.views.generic import View
-from reportlab.pdfgen import canvas
-from reportlab.lib.units import inch
-from reportlab.lib.pagesizes import letter
+import xhtml2pdf
 from django.contrib import messages
-
+from django.conf import settings
+from django.template.loader import get_template
+from xhtml2pdf import pisa
+from django.contrib.staticfiles import finders
 
 def index(request):
     # return response
@@ -75,34 +76,22 @@ def add_risks(request):
 #Creating an export to PDF option (uses ReportLab library)
 
 def export_pdf(request):
-    buf = io.BytesIO()
-    c = canvas.Canvas(buf, pagesize=letter, bottomup=0)
-    textob = c.beginText()
-    textob.setTextOrigin(inch, inch)
-    textob.setFont("Helvetica", 14)
-    risks = Risks.objects.all()
-    
-    lines = [
-        'Print this text to prove this is working'
-    ]
-    # for risk in risks:
-    #     lines.append(risks.risk_description)
-    #     lines.append(risks.risk_mitigation)
-    #     lines.append(risks.risk_owner)
-    #     lines.append(risks.risk_assignee)
-    #     lines.append(risks.risk_due_date)
-    #     lines.append(risks.risk_status)
-    # else:
-    #     print('loop is not working')
+    template_path = 'risk_details.html'
+    context = {'myvar': 'this is your template context'}
+    # Create a Django response object, and specify content_type as pdf
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="report.pdf"'
+    # find the template and render it.
+    template = get_template(template_path)
+    html = template.render(context)
 
-    for line in lines:
-        textob.textLine(line)
-
-    c.drawText(textob)
-    c.showPage()
-    c.save()
-    buf.seek(0)
-    return FileResponse(buf, as_attachment=True, filename='Risks.pdf')
+    # create a pdf
+    pisa_status = pisa.CreatePDF(
+       html, dest=response, link_callback=link_callback)
+    # if error then show some funy view
+    if pisa_status.err:
+       return HttpResponse('We had some errors <pre>' + html + '</pre>')
+    return response
 
 #This function exports CSV the risk register model into a CSV file
 def export_csv(request):
@@ -115,3 +104,35 @@ def export_csv(request):
         writer.writerow(risk)
     response['Content-Disposition']='attachement; filename=Risk-Register'+ str(datetime.datetime.now())+'.csv'
     return response
+
+#required for PDF Generation
+def link_callback(uri, rel):
+        """
+        Convert HTML URIs to absolute system paths so xhtml2pdf can access those
+        resources
+        """
+        result = finders.find(uri)
+        if result:
+                if not isinstance(result, (list, tuple)):
+                        result = [result]
+                result = list(os.path.realpath(path) for path in result)
+                path=result[0]
+        else:
+                sUrl = settings.STATIC_URL        # Typically /static/
+                sRoot = settings.STATIC_ROOT      # Typically /home/userX/project_static/
+                mUrl = settings.MEDIA_URL         # Typically /media/
+                mRoot = settings.MEDIA_ROOT       # Typically /home/userX/project_static/media/
+
+                if uri.startswith(mUrl):
+                        path = os.path.join(mRoot, uri.replace(mUrl, ""))
+                elif uri.startswith(sUrl):
+                        path = os.path.join(sRoot, uri.replace(sUrl, ""))
+                else:
+                        return uri
+
+        # make sure that file exists
+        if not os.path.isfile(path):
+                raise Exception(
+                        'media URI must start with %s or %s' % (sUrl, mUrl)
+                )
+        return path
