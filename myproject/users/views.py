e4e354c71e333cdc9d1e28736ae3b2a865498679
revisionprojects <robinmatth@live.ca>
2022-01-27 21:31:06 -0500

    adding functioning messages to user register

--- myproject/users/views.py
+++ myproject/users/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
+from pyexpat.errors import messages
+from django.shortcuts import redirect, render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
 # Create your views here.
 
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(request,f'Welcome {username}, your account has been created')
+            return redirect('index')
+    else:
+        form = UserCreationForm()
     return render (request, 'users/register.html', {'form': form}
     )
